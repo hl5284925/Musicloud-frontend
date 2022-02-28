ac13cfcde65386d495d2a7b2e414940a7e362e55
liwren <huangli1223@gmail.com>
2022-02-28 01:19:10 -0600

    updated app.component.html

--- Musicloud-Frontend/src/app/app-routing.module.ts
+++ Musicloud-Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {SearchBarComponent} from "./search-bar/search-bar.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component: HomeComponent},
+  {path:'search', component:SearchBarComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Musicloud-Frontend/src/app/home/home.component.ts
+++ Musicloud-Frontend/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,20 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  play(){
+  play() {
     console.log("play is working")
 
   }
 
-  pause(){
+  pause() {
 
   }
 
-  stop(){
+  stop() {
 
   }
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
